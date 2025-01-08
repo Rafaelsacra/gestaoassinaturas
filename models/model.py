a28c12a79a20595fdfec58e07605c68ffb2db94f
Rafaelsacra <rafaelsacra@gmail.com>
2025-01-08 17:14:53 -0300

    1assinatura

---
+++ models/model.py
@@ -0,0 +1,21 @@
+from sqlmodel import Field, SQLModel, create_engine
+from typing import Optional
+from datetime import date
+from decimal import Decimal
+
+
+class Subscription(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    empresa: str
+    site: Optional[str] = None
+    data_assinatura: date
+    valor: Decimal
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
+
+
+if __name__ == "__main__":
+   SQLModel.metadata.create_all(engine)
